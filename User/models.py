0250f3348c88655058eb3c0527a48ed49cf2924f
pranav <76247369+pranav-iitr@users.noreply.github.com>
2024-07-03 16:16:48 +0530

    bug fix

--- User/models.py
+++ User/models.py
@@ -6,7 +6,7 @@ from Backend.utils.constants import CityConstants
 class User(AbstractUser):
 
     name = models.CharField(max_length=40,blank=True,null=True)
-    phone_no = models.CharField(max_length=10, blank=True,unique=True)
+    phone_no = models.CharField(max_length=15, blank=True,unique=True)
     email = models.EmailField(_('email'), max_length=80)
     pan_card = models.CharField(max_length=10, blank=True,null=True)
     username = None
