228bda2e3f30a1c1ff9f570e19e67bc57d3a0d66
pranav <76247369+pranav-iitr@users.noreply.github.com>
2024-07-11 18:31:35 +0530

    gcp update

--- Backend/storage_backends.py
+++ Backend/storage_backends.py
@@ -1,6 +1,18 @@
-from storages.backends.s3boto3 import S3Boto3Storage
+from storages.backends.gcloud import GoogleCloudStorage
+from google.cloud import storage
+import dj_database_url
+import os
 
-class MediaStorage(S3Boto3Storage):
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+class MediaStorage(GoogleCloudStorage):
+    # authenticated users can read and write
+    storage.Client.from_service_account_json(
+        os.path.join(BASE_DIR, 'world-one.json'))
+
     location = 'media/'
     default_acl = 'private'
     file_overwrite = False
